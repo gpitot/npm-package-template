fc9ae395cfbb2a1261252e23625ec8db952e63bf
Guillaume Pitot <gpitot@nine.com.au>
2019-10-16 12:21:28 +1100

    testing config

---
+++ src/config.js
@@ -0,0 +1,36 @@
+
+
+export default {
+    sites : [
+        {
+            domain : "https://www.traveller.com.au/",
+            urls : [],
+            assets : {
+                logo : {
+                    image : "https://live-native-paid-content.pantheonsite.io/wp-content/uploads/2019/03/traveller-logo-white-01.svg",
+                    url : "https://www.traveller.com.au/"
+                },
+                back : {
+                    image : "",
+                    url : ""
+                },
+                facebook : {
+                    image : "https://live-native-paid-content.pantheonsite.io/wp-content/themes/react-theme/static/media/Facebook-white.fca9e398.svg",
+                    url : ""
+                },
+                twitter : {
+                    image : "https://live-native-paid-content.pantheonsite.io/wp-content/themes/react-theme/static/media/Twitter-white.a220a299.svg",
+                    url : ""
+                },
+                email : {
+                    image : "https://live-native-paid-content.pantheonsite.io/wp-content/themes/react-theme/static/media/Email-white.eccde6ef.svg",
+                    url : ""
+                },
+                linkedin : {
+                    image : "https://live-native-paid-content.pantheonsite.io/wp-content/themes/react-theme/static/media/LinkedIn-white.f323b719.svg",
+                    url : ""
+                }
+            }
+        }
+    ]
+}
