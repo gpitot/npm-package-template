cb2e2dedc7cddf82eb8255cc44f4033688f2619a
Guillaume Pitot <gpitot@nine.com.au>
2019-10-16 16:37:45 +1100

    urls etc

--- src/config.js
+++ src/config.js
@@ -1,36 +1,83 @@
+import backBtnSVG from "./assets/back.svg";
 
+import facebook from "./assets/facebook.svg";
+import twitter from "./assets/twitter.svg";
+import linkedin from "./assets/linkedin.svg";
+import email from "./assets/email.svg";
 
 export default {
-    sites : [
-        {
-            domain : "https://www.traveller.com.au/",
-            urls : [],
-            assets : {
-                logo : {
-                    image : "https://live-native-paid-content.pantheonsite.io/wp-content/uploads/2019/03/traveller-logo-white-01.svg",
-                    url : "https://www.traveller.com.au/"
-                },
-                back : {
-                    image : "",
-                    url : ""
-                },
-                facebook : {
-                    image : "https://live-native-paid-content.pantheonsite.io/wp-content/themes/react-theme/static/media/Facebook-white.fca9e398.svg",
-                    url : ""
-                },
-                twitter : {
-                    image : "https://live-native-paid-content.pantheonsite.io/wp-content/themes/react-theme/static/media/Twitter-white.a220a299.svg",
-                    url : ""
-                },
-                email : {
-                    image : "https://live-native-paid-content.pantheonsite.io/wp-content/themes/react-theme/static/media/Email-white.eccde6ef.svg",
-                    url : ""
-                },
-                linkedin : {
-                    image : "https://live-native-paid-content.pantheonsite.io/wp-content/themes/react-theme/static/media/LinkedIn-white.f323b719.svg",
-                    url : ""
-                }
-            }
-        }
-    ]
-}
+  sites: [
+    {
+      domain: "https://www.traveller.com.au/",
+      urls: [],
+      logo: "https://live-native-paid-content.pantheonsite.io/wp-content/uploads/2019/03/traveller-logo-black.svg"
+    },
+    {
+      domain: "https://www.afr.com",
+      urls: [],
+      logo: "https://live-native-paid-content.pantheonsite.io/wp-content/uploads/2018/03/AFR-logo.svg"
+    },
+    {
+      domain: "https://www.canberratimes.com.au",
+      urls: [],
+      logo: "https://live-native-paid-content.pantheonsite.io/wp-content/uploads/2018/08/canberra-times-logo-1.svg"
+    },
+    {
+      domain: "https://www.essentialbaby.com.au/",
+      urls: [],
+      logo: "https://live-native-paid-content.pantheonsite.io/wp-content/uploads/2018/06/essentialbaby.svg"
+    },
+    {
+      domain: "https://www.essentialkids.com.au/",
+      urls: [],
+      logo: "https://live-native-paid-content.pantheonsite.io/wp-content/uploads/2018/06/essentialkids-logo.svg"
+    },
+    {
+      domain: "http://www.executivestyle.com.au/",
+      urls: [],
+      logo: "https://live-native-paid-content.pantheonsite.io/wp-content/uploads/2018/06/executivestyle-logo.svg"
+    },
+    {
+      domain: "https://www.goodfood.com.au/",
+      urls: [],
+      logo: "https://live-native-paid-content.pantheonsite.io/wp-content/uploads/2018/06/goodfood-logo-1.svg"
+    },
+    {
+      domain: "https://www.smh.com.au",
+      urls: [],
+      logo: "https://live-native-paid-content.pantheonsite.io/wp-content/uploads/2018/06/smh-logo.svg"
+    },
+    {
+      domain: "https://www.theage.com.au",
+      urls: [],
+      logo: "https://live-native-paid-content.pantheonsite.io/wp-content/uploads/2018/08/the-age-logo-1-1.svg"
+    },
+    {
+      domain: "https://www.watoday.com.au",
+      urls: [],
+      logo: "https://live-native-paid-content.pantheonsite.io/wp-content/uploads/2018/08/wa-today-logo-1-1.svg"
+    }
+  ],
+
+  assets: {
+    back: {
+      svg: backBtnSVG
+    },
+    facebook: {
+      svg: facebook,
+      url: ""
+    },
+    twitter: {
+      svg: twitter,
+      url: ""
+    },
+    email: {
+      svg: email,
+      url: ""
+    },
+    linkedin: {
+      svg: linkedin,
+      url: ""
+    }
+  }
+};
