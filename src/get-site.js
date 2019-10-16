38e0215bdfefdd0d9135b755fb5d0bc7af22fc75
Guillaume Pitot <gpitot@nine.com.au>
2019-10-16 16:38:01 +1100

    debug option

--- src/get-site.js
+++ src/get-site.js
@@ -1,11 +1,16 @@
 /* get domain */
 import config from "./config";
 
-export default () => {
+export default debug => {
   const hostname = window.location.hostname || window.location.host;
   if (!hostname) return null;
-  const {sites} = config;
-  if (hostname.includes('localhost')) return sites[0];
+  const { sites } = config;
+
+  if (debug !== false) {
+      console.log('debug');
+      return sites[debug];
+  }
+
   for (let i = 0; i < sites.length; i += 1) {
     for (let x = 0; x < sites[i].urls; x += 1) {
       if (hostname.includes(sites[i].urls[x])) return sites[i];
