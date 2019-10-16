46a1bbfde91e097de4dbfe862a4f75b24e0b8ab9
Guillaume Pitot <gpitot@nine.com.au>
2019-10-16 12:21:48 +1100

    setup

---
+++ src/setup-footer.js
@@ -0,0 +1,14 @@
+
+
+
+
+export default () => {
+
+    const footer = document.querySelector('footer');
+    if (!footer) return;
+
+    footer.style.height="300px";
+    footer.style.background= "blue";
+
+
+}
---
+++ src/setup-header.js
@@ -0,0 +1,13 @@
+
+
+
+export default () => {
+
+    const header = document.querySelector('header');
+    if (!header) return;
+
+    header.style.height="300px";
+    header.style.background= "red";
+
+
+}
